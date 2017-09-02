0c7cf527b2799a68dfb3675b6741fc55f9aadaaa
DaWinchi <lehaprutskov@gmail.com>
2017-09-02 23:44:08 +0300

    auth_complete

--- Auth.cs
+++ Auth.cs
@@ -19,7 +19,7 @@ namespace VKPlayer
 
         private void Auth_Load(object sender, EventArgs e)
         {
-            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=6171434&scope=audio&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.68response_type=token");
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=6171434&scope=audio&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.68&response_type=token");
         }
 
         private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
@@ -30,6 +30,22 @@ namespace VKPlayer
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             toolStripStatusLabel1.Text = "Загружено";
+            if (webBrowser1.Url.ToString().Contains('#'))
+                {
+                    string URL = webBrowser1.Url.ToString();
+                    string l = URL.Split('#')[1];
+                    if (l[0] == 'a')
+                    {
+                        Settings.Default.token = l.Split('&')[0].Split('=')[1];
+                        Settings.Default.id = l.Split('=')[3];
+                        Settings.Default.auth = true;
+                        MessageBox.Show(Settings.Default.token + " " + Settings.Default.id);
+                        this.Close();
+                    }
+                }
+           
         }
+
+      
     }
 }
