6cffb7254a70ecc4735407741464cce5a8c90218
DaWinchi <lehaprutskov@gmail.com>
2017-09-02 22:43:54 +0300

    vk_auth

---
+++ Auth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VKPlayer
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=6171434&scope=audio&redirect_uri=https://oauth.vk.com/blank.html&display=popup&v=5.68response_type=token");
+        }
+    }
+}
